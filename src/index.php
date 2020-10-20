2f993ff4ddcd535c79d7cda5a765ab56ac3288bc
Tony <tony.tiryaki@etud.u-picardie.fr>
2020-10-20 17:39:17 +0200

    je viens de faire une modification sur le fichier index.php
    Ainsi que ajout du fichier hola.txt

--- src/index.php
+++ src/index.php
@@ -1,1 +1,15 @@
-blablabla
+
+# Veuillez saisir le message de validation pour vos modifications. Les lignes
+# commençant par '#' seront ignorées, et un message vide abandonne la validation.
+#
+# Sur la branche main
+# Votre branche est à jour avec 'origin/main'.
+#
+# Modifications qui seront validées :
+#	nouveau fichier : src/index.php
+#
+# Modifications qui ne seront pas validées :
+#	supprimé :        index.php
+#
+
+I CAME IN LIKE A WREEEEECKING BAAAAAAAAAAAAAALL
